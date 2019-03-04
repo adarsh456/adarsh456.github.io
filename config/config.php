4a6f33847b0d09c2647632dadc825f771c05c6cf
adarsh456 <38810740+adarsh456@users.noreply.github.com>
2019-03-04 18:35:56 +0530

    new website

--- config/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-    define('ROOT_URL','http://localhost:8081/oldcar/');
-?>
--- sesdes.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["mid"]);  // where $_SESSION["nome"] is your own variable. if you do not have one use only this as follow **session_unset();**
-session_destroy();
-header("Location: index.php");
-?>
